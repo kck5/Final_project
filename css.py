33c7484a5fee71eca1f0b439bcea1fd5c1014f8d
SakethdotK <67869067+SakethKudupudi@users.noreply.github.com>
2023-12-06 01:17:46 +0530

    Add files via upload

---
+++ css.py
@@ -0,0 +1,11 @@
+css_string = """
+             <style>
+
+            .sidebar {max-width:100%; float:right}
+            .side-block img {width:100%}
+            .side-block {margin-bottom:15px}
+            .side-block a.button {margin-bottom:30px; background: #006CFF; color: #fff;font-weight: 500;font-size: 16px;line-height: 20px;padding: 15px;display: inline-block;max-width: 300px;border-radius: 5px; text-decoration:none;   }
+            .side-block a.button:hover {background:#000}
+
+            </style> 
+            """

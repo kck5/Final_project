f5bcfd380494a5487c777d94c1139779f928cf40
SakethdotK <67869067+SakethKudupudi@users.noreply.github.com>
2023-12-06 12:56:25 +0530

    Update drowsy_detection.py

--- drowsy_detection.py
+++ drowsy_detection.py
@@ -11,7 +11,7 @@ def get_mediapipe_app(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5,
 ):
-    """Mediapipe"""
+    """Initialize and return Mediapipe FaceMesh Solution Graph object"""
     face_mesh = mp.solutions.face_mesh.FaceMesh(
         max_num_faces=max_num_faces,
         refine_landmarks=refine_landmarks,
@@ -23,25 +23,40 @@ def get_mediapipe_app(
 
 
 def distance(point_1, point_2):
+    """Calculate l2-norm between two points"""
     dist = sum([(i - j) ** 2 for i, j in zip(point_1, point_2)]) ** 0.5
     return dist
 
 
 def get_ear(landmarks, refer_idxs, frame_width, frame_height):
-    
+    """
+    Calculate Eye Aspect Ratio for one eye.
+
+    Args:
+        landmarks: (list) Detected landmarks list
+        refer_idxs: (list) Index positions of the chosen landmarks
+                            in order P1, P2, P3, P4, P5, P6
+
+        frame_width: (int) Width of captured frame
+        frame_height: (int) Height of captured frame
+
+    Returns:
+        ear: (float) Eye aspect ratio
+    """
     try:
+        # Compute the euclidean distance between the horizontal
         coords_points = []
         for i in refer_idxs:
             lm = landmarks[i]
             coord = denormalize_coordinates(lm.x, lm.y, frame_width, frame_height)
             coords_points.append(coord)
 
-      
+        # Eye landmark (x, y)-coordinates
         P2_P6 = distance(coords_points[1], coords_points[5])
         P3_P5 = distance(coords_points[2], coords_points[4])
         P1_P4 = distance(coords_points[0], coords_points[3])
 
-        
+        # Compute the eye aspect ratio
         ear = (P2_P6 + P3_P5) / (2.0 * P1_P4)
 
     except:
@@ -52,6 +67,8 @@ def get_ear(landmarks, refer_idxs, frame_width, frame_height):
 
 
 def calculate_avg_ear(landmarks, left_eye_idxs, right_eye_idxs, image_w, image_h):
+    # Calculate Eye aspect ratio
+
     left_ear, left_lm_coordinates = get_ear(landmarks, left_eye_idxs, image_w, image_h)
     right_ear, right_lm_coordinates = get_ear(landmarks, right_eye_idxs, image_w, image_h)
     Avg_EAR = (left_ear + right_ear) / 2.0
@@ -76,21 +93,28 @@ def plot_text(image, text, origin, color, font=cv2.FONT_HERSHEY_SIMPLEX, fntScal
 
 class VideoFrameHandler:
     def __init__(self):
-              
+        """
+        Initialize the necessary constants, mediapipe app
+        and tracker variables
+        """
+        # Left and right eye chosen landmarks.
         self.eye_idxs = {
             "left": [362, 385, 387, 263, 373, 380],
             "right": [33, 160, 158, 133, 153, 144],
         }
 
+        # Used for coloring landmark points.
+        # Its value depends on the current EAR value.
         self.RED = (0, 0, 255)  # BGR
         self.GREEN = (0, 255, 0)  # BGR
 
+        # Initializing Mediapipe FaceMesh solution pipeline
         self.facemesh_model = get_mediapipe_app()
 
-       
+        # For tracking counters and sharing states in and out of callbacks.
         self.state_tracker = {
             "start_time": time.perf_counter(),
-            "DROWSY_TIME": 0.0,  
+            "DROWSY_TIME": 0.0,  # Holds the amount of time passed with EAR < EAR_THRESH
             "COLOR": self.GREEN,
             "play_alarm": False,
         }
@@ -111,7 +135,8 @@ class VideoFrameHandler:
             indicate if the alarm should be played or not.
         """
 
-        
+        # To improve performance,
+        # mark the frame as not writeable to pass by reference.
         frame.flags.writeable = False
         frame_h, frame_w, _ = frame.shape
 
@@ -126,7 +151,9 @@ class VideoFrameHandler:
             frame = plot_eye_landmarks(frame, coordinates[0], coordinates[1], self.state_tracker["COLOR"])
 
             if EAR < thresholds["EAR_THRESH"]:
-  
+
+                # Increase DROWSY_TIME to track the time period with EAR less than threshold
+                # and reset the start_time for the next iteration.
                 end_time = time.perf_counter()
 
                 self.state_tracker["DROWSY_TIME"] += end_time - self.state_tracker["start_time"]
@@ -154,3 +181,7 @@ class VideoFrameHandler:
             self.state_tracker["COLOR"] = self.GREEN
             self.state_tracker["play_alarm"] = False
 
+            # Flip the frame horizontally for a selfie-view display.
+            frame = cv2.flip(frame, 1)
+
+        return frame, self.state_tracker["play_alarm"]

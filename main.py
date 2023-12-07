dd61eb3cce4a253c5a74596f7c984d12efc35654
K.Chandrakanth <90635317+kck5@users.noreply.github.com>
2023-12-07 14:01:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,52 @@
-import os
-import av
-import threading
 import streamlit as st
-import streamlit_nested_layout
-from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
+import cv2
+import mediapipe as mp
 
-from audio_handling import AudioFrameHandler
-from drowsy_detection import VideoFrameHandler
-from css import css_string
+# Initialize mediapipe
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
+# Define functions
+def process_frame(frame):
+    # Hand detection
+    results = hands.process(frame)
+    hand_landmarks = results.multi_hand_landmarks
 
-# Define the audio file to use.
-alarm_file_path = os.path.join("audio", "wake_up.wav")
+    # Check for detected hand
+    if hand_landmarks:
+        # Extract landmark points
+        landmark_list = hand_landmarks[0].landmark
+        # Calculate object dimensions (replace with specific logic)
+        dimensions = calculate_dimensions(landmark_list)
+        # Display dimensions
+        st.write(f"Object dimensions: {dimensions}")
 
-# Streamlit Components
-st.set_page_config(
-    page_title="Drowsiness Detection",
-    
-    layout="wide",  # centered, wide
-    initial_sidebar_state="expanded",
-    
-)
+        # Optionally draw hand landmarks
+        mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
+    return frame
 
-col1, col2 = st.columns(spec=[6, 2], gap="medium")
+# Main app
+def main():
+    # Initialize video capture
+    cap = cv2.VideoCapture(0)
 
-with col1:
-    st.title("Major Project- Drowsiness Detection")
-    with st.container():
-        c1, c2 = st.columns(spec=[1, 1])
-        with c1:
-            # The amount of time (in seconds) to wait before sounding the alarm.
-            WAIT_TIME = st.slider("Seconds to wait before sounding alarm:", 0.0, 5.0, 1.0, 0.25)
+    while True:
+        # Capture frame
+        success, frame = cap.read()
 
-        with c2:
-            # Lowest valid value of Eye Aspect Ratio. Ideal values [0.15, 0.2].
-            EAR_THRESH = st.slider("Eye Aspect Ratio threshold:", 0.0, 0.4, 0.18, 0.01)
+        # Process frame and get dimensions
+        frame = process_frame(frame)
 
-thresholds = {
-    "EAR_THRESH": EAR_THRESH,
-    "WAIT_TIME": WAIT_TIME,
-}
+        # Display video
+        st.image(frame)
 
-# For streamlit-webrtc
-video_handler = VideoFrameHandler()
-audio_handler = AudioFrameHandler(sound_file_path=alarm_file_path)
+        # Close on 'q' key press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-lock = threading.Lock()  # For thread-safe access & to prevent race-condition.
-shared_state = {"play_alarm": False}
+    cap.release()
+    cv2.destroyAllWindows()
 
-
-def video_frame_callback(frame: av.VideoFrame):
-    frame = frame.to_ndarray(format="bgr24")  # Decode and convert frame to RGB
-
-    frame, play_alarm = video_handler.process(frame, thresholds)  # Process frame
-    with lock:
-        shared_state["play_alarm"] = play_alarm  # Update shared state
-
-    return av.VideoFrame.from_ndarray(frame, format="bgr24")  # Encode and return BGR frame
-
-
-def audio_frame_callback(frame: av.AudioFrame):
-    with lock:  # access the current “play_alarm” state
-        play_alarm = shared_state["play_alarm"]
-
-    new_frame: av.AudioFrame = audio_handler.process(frame, play_sound=play_alarm)
-    return new_frame
-
-
-# https://github.com/whitphx/streamlit-webrtc/blob/main/streamlit_webrtc/config.py
-
-with col1:
-    ctx = webrtc_streamer(
-        key="drowsiness-detection",
-        video_frame_callback=video_frame_callback,
-        audio_frame_callback=audio_frame_callback,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this to config for cloud deployment.
-        media_stream_constraints={"video": {"height": {"ideal": 480}}, "audio": True},
-        video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
-    )
+if __name__ == "__main__":
+    main()
